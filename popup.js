f2c3c74482d6fc28580f4979f0800eb52dded9ba
bodrulamin <bodrulaminiu@gmail.com>
2024-09-18 20:14:32 +0600

    update popup html

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var version = 9;
 
-localStorage.setItem("version",version);
+localStorage.setItem("version", version);
 
 document.addEventListener('DOMContentLoaded', function () {
     const quickMenutoggle = document.getElementById('quickMenutoggle');
@@ -38,28 +38,37 @@ document.addEventListener('DOMContentLoaded', function () {
     var btn = document.getElementById('versionInfoBtn');
     btn.onclick = fetchVersionInfo;
 
+    var gotoMITSBtn = document.getElementById('gotoMITSBtn');
+    var mitsNumberInput = document.getElementById('mitsNumberInput');
+
+    gotoMITSBtn.onclick = ()=>{
+        const mitsNumber = mitsNumberInput.value;
+        const url = `http://192.168.1.126:1234/mantis/view.php?id=${mitsNumber}`;
+        chrome.tabs.create({ url: url });
+    };
+  
 });
 
 function fetchVersionInfo() {
     const url = 'https://raw.githubusercontent.com/bodrulamin/misl-tools/master/version.json'; // Replace with your API endpoint
 
     fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        
-        return response.json(); // Parse the JSON from the response
-      })
-      .then(data => {
-        console.log(data);
-        if(+localStorage.getItem('version') < data.version ) {
-            var updateAlert = document.getElementById('updateAlert');
-            updateAlert.classList.remove('d-none');
-            updateAlert.classList.add('d-block');
-        }
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+
+            return response.json(); // Parse the JSON from the response
+        })
+        .then(data => {
+            console.log(data);
+            if (+localStorage.getItem('version') < data.version) {
+                var updateAlert = document.getElementById('updateAlert');
+                updateAlert.classList.remove('d-none');
+                updateAlert.classList.add('d-block');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
