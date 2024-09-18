14c99016e2830ea60d1eb655fa5785cd93adee46
bodrulamin <bodrulaminiu@gmail.com>
2024-09-18 09:35:51 +0600

    added version info

--- popup.js
+++ popup.js
@@ -31,4 +31,23 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+    const url = 'https://raw.githubusercontent.com/bodrulamin/misl-tools/master/version.json'; // Replace with your API endpoint
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        
+        return response.json(); // Parse the JSON from the response
+      })
+      .then(data => {
+        console.log(data); 
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+
+
 });
