ad6e43c6ee432064931e0ef4c12978b8f0795c66
bodrulamin <bodrulaminiu@gmail.com>
2024-11-20 17:45:27 +0600

    highlight current open bugs

--- content.js
+++ content.js
@@ -23,10 +23,10 @@ document.addEventListener('mouseup', detectTextSelection);
 document.addEventListener('oncontextmenu', onContextMenuOpen);
 
 if (
-    window.location.href.includes('/mantis/view.php?id=') || 
-    window.location.href.includes('bugnote_edit_page.php') || 
-    window.location.href.includes('bug_update_page.php') || 
-    window.location.href.includes('support.ababilng.com/view.php?id') || 
+    window.location.href.includes('/mantis/view.php?id=') ||
+    window.location.href.includes('bugnote_edit_page.php') ||
+    window.location.href.includes('bug_update_page.php') ||
+    window.location.href.includes('support.ababilng.com/view.php?id') ||
     window.location.href.includes('bug_report_page.php')) {
 
     const script = document.createElement('script');
@@ -39,7 +39,7 @@ if (
     setTimeout(() => {
         const script2 = document.createElement('script');
         script2.type = "text/javascript"
-        script2.src = chrome.runtime.getURL('initTinymce.js');
+        script2.src = chrome.runtime.getURL('scripts/initTinymce.js');
         script2.onload = () => {
         };
         (document.head || document.documentElement).appendChild(script2);
@@ -47,7 +47,21 @@ if (
     }, 100)
 }
 
+if (window.location.href.includes('bug_update_page.php')) {
+    injectScript('scripts/date_fields.js');
+}
 
+function injectScript(relativeUrl) {
+    setTimeout(() => {
+        let script2 = document.createElement('script');
+        script2.type = "text/javascript"
+        script2.src = chrome.runtime.getURL(relativeUrl);
+        script2.onload = () => {
+        };
+        (document.head || document.documentElement).appendChild(script2);
+
+    }, 100)
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'hide') {
